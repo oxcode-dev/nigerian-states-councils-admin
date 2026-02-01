9424641bd462eed66d96e2c876cb807b9a2a5a52
Oxcode <mrexcelsam1@gmail.com>
2026-02-02 00:53:48 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Login from "./pages/Login"
 import ForgotPassword from "./pages/ForgotPassword"
+import ProtectedRoute from "./components/ProtectedRoutes"
 
 function App() {
 
@@ -12,6 +13,14 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route
+          path="/*"
+          element={
+            <ProtectedRoute>
+              <AdminRoutes />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
      </Router>
     </>
@@ -23,6 +32,17 @@ const Home = () => {
     <div>
       <h1>Welcome to the Home Page</h1>
     </div>
+  )
+}
+
+const AdminRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      {/* <Route path="/product/create" element={<CreateProduct />} />
+      <Route path="/product/edit/:id" element={<EditProduct />} />
+      <Route path="/product/delete/:id" element={<DeleteProduct />} /> */}
+    </Routes>
   )
 }
 
