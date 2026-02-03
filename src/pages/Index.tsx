057e3127d1842320be3c98c4aed1bd9d10f30727
Oxcode <mrexcelsam1@gmail.com>
2026-02-03 23:41:53 +0100

    initial commit

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,16 @@
+// import { useQuery } from "@tanstack/react-query";
 import Layout from "../layout";
+// import axios from 'axios'
 
 export default function Home() {
+    // const fetchUsers = async () => {
+    //     const { data } = await axios('/users', {
+    //         withCredentials: true
+    //     });
+    //     return data;
+    // }
+
+    // const { data, error, isLoading} = useQuery('users', fetchUsers)
     return (
         <Layout>
             Hello World
