7cc78f3406c8c22ca2089e6a5866e36790b1bc9a
Oxcode <mrexcelsam1@gmail.com>
2026-02-03 10:28:58 +0100

    initial commit

--- src/contexts/ReactQueryProvider.tsx
+++ src/contexts/ReactQueryProvider.tsx
@@ -30,7 +30,7 @@ function getQueryClient() {
   }
 }
 
-export default function Providers({ children }: { children: React.ReactNode }) {
+export default function ReactQueryProvider({ children }: { children: React.ReactNode }) {
   const queryClient = getQueryClient();
   return (
     <QueryClientProvider client={queryClient}>
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ToastContextProvider } from './contexts/ToastContext.tsx'
 import ToastNotification from './components/ToastNotification.tsx'
+import ReactQueryProvider from './contexts/ReactQueryProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ToastContextProvider>
-      <ToastNotification />
-      <App />
-    </ToastContextProvider>
+    <ReactQueryProvider>
+      <ToastContextProvider>
+        <ToastNotification />
+        <App />
+      </ToastContextProvider>
+    </ReactQueryProvider>
   </StrictMode>,
 )
