fcf040ad0804cd8450164a5af7694870c657fde5
Oxcode <mrexcelsam1@gmail.com>
2026-02-05 00:35:56 +0100

    initial commit

--- src/components/ConfirmDeleteModal.tsx
+++ src/components/ConfirmDeleteModal.tsx
@@ -1,3 +1,4 @@
+import { ExclamationCircleIcon, XMarkIcon } from "@heroicons/react/20/solid";
 import React, { useEffect, useRef } from "react";
 
 type ConfirmDeleteModalProps = {
@@ -39,20 +40,7 @@ const ConfirmDeleteModal: React.FC<ConfirmDeleteModalProps> = ({
         <div className="relative block w-full max-w-lg rounded-lg bg-red-500 px-4 py-2 text-white">
           {/* Icon */}
           <div className="absolute top-4 left-4">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-8 h-8"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
-              />
-            </svg>
+            <ExclamationCircleIcon className="h-8 w-8" />
           </div>
 
           {/* Content */}
@@ -79,25 +67,12 @@ const ConfirmDeleteModal: React.FC<ConfirmDeleteModalProps> = ({
           </div>
 
           {/* Close button */}
-          <div className="absolute top-3 right-3 w-max rounded-lg transition-all hover:bg-white hover:bg-opacity-20">
+          <div className="absolute top-3 right-3 w-max rounded-lg transition-all hover:text-red-500 hover:bg-white hover:bg-opacity-20">
             <button
               onClick={() => onClose(false)}
               className="w-max rounded-lg p-1"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
+              <XMarkIcon className="h-6 w-6" />
             </button>
           </div>
         </div>
--- src/pages/States.tsx
+++ src/pages/States.tsx
@@ -6,15 +6,65 @@ import StateForm from "../forms/StateForm";
 import { useState } from "react";
 import type { StateFormProp } from "../types";
 import ConfirmDeleteModal from "../components/ConfirmDeleteModal";
+import { API_BASE_URL, STATES_QUERY_KEY } from "../constants";
+import { useToastContext } from "../contexts/ToastContext";
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 export default function States() {
     const { states, isFetching } = useFetchStates()
     const [selectedState, setSelectedState] = useState<StateFormProp | null>(null)
     const [isFormOpen, setIsFormOpen] = useState(false) 
+    const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false)
+
+    const { showToast } = useToastContext()
+    
+    const queryClient = useQueryClient()
 
     const handleEdit = (state: StateFormProp) => {
         setSelectedState(state)
         setIsFormOpen(true)
+    }
+
+    const handleDelete = (state: StateFormProp) => {
+        setSelectedState(state)
+        setIsDeleteModalOpen(true)
+    }
+
+    const mutation = useMutation({
+        mutationFn: () => handleDeleteConfirm()
+    })
+
+    const onDeleteState = async() => {
+        mutation.mutate()
+    }
+
+    const handleDeleteConfirm = async() => {
+        console.log('Delete confirmed for', selectedState);
+
+        const url = `${API_BASE_URL}/states/${selectedState?._id}`
+        
+        const response = await fetch(url, {
+            method: 'DELETE',
+            headers: { 
+                // Authorization: `Bearer ${getToken.token}`,
+                'Content-Type': 'application/json',
+                'Accept': 'application/json', 
+            },
+            body: JSON.stringify({ 
+                _id: selectedState?._id,
+            }),
+        })
+
+        const feedback = await response.json()
+
+        if (feedback?.status === 'success') {
+            queryClient.invalidateQueries({ queryKey: [STATES_QUERY_KEY] })
+            showToast('Success', feedback?.message || 'State deleted successfully', 'success', true, 10)
+            setIsDeleteModalOpen(false)
+        } else {
+            showToast('Error Occurred', feedback?.message || 'An error occurred', 'error', true, 10)
+            setIsDeleteModalOpen(false)
+        }
     }
 
     return (
@@ -42,13 +92,13 @@ export default function States() {
                                             </th>
                                             <th
                                                 scope="col"
-                                                className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                                                className="text-sm font-medium text-gray-900 px-6 py-4 text-left hidden md:table-cell"
                                             >
                                                 Geo Zone
                                             </th>
                                             <th
                                                 scope="col"
-                                                className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                                                className="text-sm font-medium text-gray-900 px-6 py-4 text-left hidden md:table-cell"
                                             >
                                                 Capital City
                                             </th>
@@ -64,10 +114,10 @@ export default function States() {
                                                 <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                                     {state.name}
                                                 </td>
-                                                <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                                <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap hidden md:table-cell">
                                                     {state.geo_zone}
                                                 </td>
-                                                <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                                <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap hidden md:table-cell">
                                                     {state.capital_city}
                                                 </td>
                                                 <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
@@ -75,7 +125,7 @@ export default function States() {
                                                         <button onClick={() => handleEdit(state)} className="w-6 h-6 bg-blue-600 text-white rounded p-1 cursor-pointer">
                                                             <PencilIcon className="size-4" />
                                                         </button>
-                                                        <button className="w-6 h-6 bg-red-600 text-white rounded p-1 cursor-pointer">
+                                                        <button onClick={() => handleDelete(state)} className="w-6 h-6 bg-red-600 text-white rounded p-1 cursor-pointer">
                                                             <TrashIcon className="size-4" />
                                                         </button>
                                                     </div>
@@ -96,11 +146,14 @@ export default function States() {
                         state={selectedState}
                     />
                 }
-                <ConfirmDeleteModal 
-                    active={true} 
-                    message="Are you sure you want to delete this state?" 
-                    submitFn={() => console.log('Deleted')} onClose={() => console.log('Closed')}
-                />
+                { isDeleteModalOpen && 
+                    <ConfirmDeleteModal 
+                        active={isDeleteModalOpen} 
+                        message="Are you sure you want to delete this state?" 
+                        submitFn={onDeleteState} 
+                        onClose={() => setIsDeleteModalOpen(false)}
+                    />
+                }
             </main>
 
         </Layout>
