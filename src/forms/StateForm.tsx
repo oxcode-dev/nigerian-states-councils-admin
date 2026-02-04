43e6b7cb602729069e7b6ee5e4321cfa447de92d
Oxcode <mrexcelsam1@gmail.com>
2026-02-04 10:17:17 +0100

    initial commit

--- src/forms/StateForm.tsx
+++ src/forms/StateForm.tsx
@@ -1,56 +1,60 @@
 'use client';
 
 import { Dialog,  DialogContent, DialogTitle, DialogTrigger } from '../components/Dialog';
-
-
-// import ErrorAlert from '@/components/ErrorAlert';
-// import { isEmpty } from '@/types/helper';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import React, { FormEvent, useState } from 'react'
-import { useForm, SubmitHandler } from "react-hook-form"
+import React, { useState } from 'react'
+import { useForm } from "react-hook-form"
 
-type AddressFormProp = {
-    street?: string
-    city?: string
-    state?: string
-    postal_code?: string,
-    id?: string | null
+type StateFormProp = {
+    name: string
+    geo_zone: string
+    code: string
+    capital_city: string
+    slogan: string
+    description: string
+    creation_year: number
+    created_at: string
+    _id?: string | null
 };
 
 
 type FormProp = {
+    state: StateFormProp | null;
     open: boolean;
     setOpen: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-const StateForm = ({ open, setOpen} : FormProp) => {
+const StateForm = ({ open, setOpen, state } : FormProp) => {
 
     const [errorBag, setErrorBag] = useState({})
 
     const queryClient = useQueryClient()
+
     const mutation = useMutation({
-        mutationFn: (data: AddressFormProp) => handleForm(data)
+        mutationFn: (data: StateFormProp) => handleForm(data)
     })
 
     const {
         register,
         handleSubmit,
         formState: { errors },
-    } = useForm<AddressFormProp>({
+    } = useForm<StateFormProp>({
         defaultValues: {
-            city: address?.city || '',
-            street: address?.street || '',
-            state: address?.state || '',
-            postal_code: address?.postal_code || '',
-            id: address?.id || '',
+            name: state?.name || '',
+            geo_zone: state?.geo_zone || '',
+            code: state?.code || '',
+            capital_city: state?.capital_city || '',
+            slogan: state?.slogan || '',
+            description: state?.description || '',
+            creation_year: state?.creation_year || 0,
         }
     });
 
-    const onSubmit: SubmitHandler<AddressFormProp> = async(data) => {
+    const onSubmit = async(data: StateFormProp) => {
         mutation.mutate(data)
     }
 
-    const handleForm = async (data: AddressFormProp) => {
+    const handleForm = async (data: StateFormProp) => {
         const getTokenResponse = await fetch('/api/fetch-token')
 
         const getToken = await getTokenResponse.json()
@@ -59,20 +63,23 @@ const StateForm = ({ open, setOpen} : FormProp) => {
             return alert('Unauthenticated User')
         }
 
-        const url = `${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/addresses${data.id ? '/' + data.id : ''}`
+        const url = `${import.meta.env.VITE_REACT_APP_BACKEND_BASEURL}/states${data._id ? '/' + data._id : ''}`
 
         const response = await fetch(url, {
-            method: data.id ? 'PUT' : 'POST',
+            method: data._id ? 'PUT' : 'POST',
             headers: { 
                 Authorization: `Bearer ${getToken.token}`,
                 'Content-Type': 'application/json',
                 'Accept': 'application/json', 
             },
             body: JSON.stringify({ 
-                street: data?.street,
-                city: data?.city,
-                state: data?.state,
-                postal_code: data?.postal_code,
+                name: data?.name,
+                geo_zone: data?.geo_zone,
+                code: data?.code,
+                capital_city: data?.capital_city,
+                slogan: data?.slogan,
+                description: data?.description,
+                creation_year: data?.creation_year,
             }),
         })
 
@@ -89,7 +96,7 @@ const StateForm = ({ open, setOpen} : FormProp) => {
     }
 
     return (
-        <div>
+        <div className="relative my-8 overflow-hidden">
             <Dialog open={open} onOpenChange={setOpen}>
                 <DialogTrigger asChild>
                     <button className="btn rounded-md bg-blue-500 text-white border border-blue-500 font-medium">
@@ -97,44 +104,52 @@ const StateForm = ({ open, setOpen} : FormProp) => {
                         <span>Create</span>
                     </button>
                 </DialogTrigger>
-                <div className="">
+                <div className="overflow-auto">
                     <DialogContent className="!bg-white">
                         <DialogTitle>Create New Address</DialogTitle>
                         
                         <div>
                             <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 pt-6">
-                                { !isEmpty(errorBag) ? (
-                                    <ErrorAlert>
-                                        <ol className="inline-flex flex-col space-y-1.5 list-disc list-inside">
-                                            { Object.values(errorBag).map((value, key) => (
-                                                //@ts-ignore
-                                                <li key={key}>{value}</li>
-                                            ))}
-                                        </ol>
-                                    </ErrorAlert>
-                                ) : null}
 
                                 <div className="space-y-1.5 flex flex-col">
-                                    <label className="text-sm font-medium text-gray-800" htmlFor="street">Street</label>
-                                    <input type="text" {...register("street",  { required: true })}  placeholder="Street" className="input w-full bg-white border border-gray-300" />
-                                    {errors.street && <span className="text-red-600 text-xs font-medium">Street is required</span>}
+                                    <label className="text-sm font-medium text-gray-800" htmlFor="name">Name</label>
+                                    <input type="text" {...register("name",  { required: true })}  placeholder="Name" className="input w-full bg-white border border-gray-300" />
+                                    {errors.name && <span className="text-red-600 text-xs font-medium">Name is required</span>}
                                 </div>
                                 <div className="space-y-1.5 flex flex-col">
-                                    <label className="text-sm font-medium text-gray-800" htmlFor="city">City</label>
-                                    <input type="text" {...register("city",  { required: true })} placeholder="City" className="input w-full bg-white border border-gray-300" />
-                                    {errors.city && <span className="text-red-600 text-xs font-medium">City is required</span>}
+                                    <label className="text-sm font-medium text-gray-800" htmlFor="geo_zone">Geo Zone</label>
+                                    <input type="text" {...register("geo_zone",  { required: true })} placeholder="Geo Zone" className="input w-full bg-white border border-gray-300" />
+                                    {errors.geo_zone && <span className="text-red-600 text-xs font-medium">Geo Zone is required</span>}
                                 </div>
 
                                 <div className="space-y-1.5 flex flex-col">
-                                    <label className="text-sm font-medium text-gray-800" htmlFor="state">State</label>
-                                    <input type="text" {...register("state",  { required: true })} placeholder="Your State" className="input w-full bg-white border border-gray-300" />
-                                    {errors.state && <span className="text-red-600 text-xs font-medium">State is required</span>}
+                                    <label className="text-sm font-medium text-gray-800" htmlFor="code">Code</label>
+                                    <input type="text" {...register("code",  { required: true })} placeholder="Code" className="input w-full bg-white border border-gray-300" />
+                                    {errors.code && <span className="text-red-600 text-xs font-medium">Code is required</span>}
                                 </div>
 
                                 <div className="space-y-1.5 flex flex-col">
-                                    <label className="text-sm font-medium text-gray-800" htmlFor="postal_code">Postal Code</label>
-                                    <input id="postal_code" type="text" {...register("postal_code",  { required: true })} placeholder="Postal Code" className="input w-full bg-white border border-gray-300" />
-                                    {errors.postal_code && <span className="text-red-600 text-xs font-medium">Postal Code is required</span>}
+                                    <label className="text-sm font-medium text-gray-800" htmlFor="capital_city">Capital City</label>
+                                    <input id="capital_city" type="text" {...register("capital_city",  { required: true })} placeholder="Capital City" className="input w-full bg-white border border-gray-300" />
+                                    {errors.capital_city && <span className="text-red-600 text-xs font-medium">Capital City is required</span>}
+                                </div>
+
+                                <div className="space-y-1.5 flex flex-col">
+                                    <label className="text-sm font-medium text-gray-800" htmlFor="slogan">Slogan</label>
+                                    <input id="slogan" type="text" {...register("slogan",  { required: true })} placeholder="Slogan" className="input w-full bg-white border border-gray-300" />
+                                    {errors.slogan && <span className="text-red-600 text-xs font-medium">Slogan is required</span>}
+                                </div>
+
+                                <div className="space-y-1.5 flex flex-col">
+                                    <label className="text-sm font-medium text-gray-800" htmlFor="description">Description</label>
+                                    <input id="description" type="text" {...register("description",  { required: true })} placeholder="Description" className="input w-full bg-white border border-gray-300" />
+                                    {errors.description && <span className="text-red-600 text-xs font-medium">Description is required</span>}
+                                </div>
+
+                                <div className="space-y-1.5 flex flex-col">
+                                    <label className="text-sm font-medium text-gray-800" htmlFor="creation_year">Creation Year</label>
+                                    <input id="creation_year" type="text" {...register("creation_year",  { required: true })} placeholder="Creation Year" className="input w-full bg-white border border-gray-300" />
+                                    {errors.creation_year && <span className="text-red-600 text-xs font-medium">Creation Year is required</span>}
                                 </div>
 
                                 <div className="py-2 pt-6 space-x-3">
