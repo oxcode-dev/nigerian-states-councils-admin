4b48cc6b820c42caaf62b80e13f1e42f72fd1cf9
Oxcode <mrexcelsam1@gmail.com>
2026-02-06 15:09:09 +0100

    initial commit

--- src/pages/LGAs.tsx
+++ src/pages/LGAs.tsx
@@ -144,16 +144,24 @@ export default function LocalGovts() {
                                     </table>
                                 </div>
                             </div>
-                            <pre>{JSON.stringify(metaData, null, 2)}</pre>
-                            <div className="w-full mx-auto flex justify-center mb-4">
+                            {/* <pre>{JSON.stringify(metaData, null, 2)}</pre> */}
+                            <div className="w-full mx-auto flex justify-center my-4 mt-6">
                                 <div className="join">
-                                    <button disabled={1 === 1} className="join-item btn bg-gray-200 text-gray-500 border border-gray-200 disabled:opacity-50">
+                                    <button 
+                                        disabled={metaData?.page === 1} 
+                                        onClick={() => setPage(metaData?.page - 1)} 
+                                        className="join-item btn bg-gray-200 text-gray-500 border border-gray-200 disabled:opacity-50"
+                                    >
                                         <ChevronLeftIcon className="size-6" />
                                     </button>
-                                    <div className="join-item btn bg-gray-200 text-gray-500 border border-gray-200">
-                                        Page 1
+                                    <div className="join-item font-light btn bg-gray-200 text-gray-500 border border-gray-200">
+                                        Page <b className="font-bold text-md">{metaData?.page || 0}</b> of {metaData?.totalPages || 0}
                                     </div>
-                                    <button onClick={() => setPage(1 + metaData?.page || 0)} className="join-item btn bg-gray-200 text-gray-500 border border-gray-200">
+                                    <button 
+                                        disabled={metaData?.page === metaData?.totalPages}
+                                        onClick={() => setPage(1 + metaData?.page || 0)} 
+                                        className="join-item btn bg-gray-200 text-gray-500 border border-gray-200 disabled:opacity-50"
+                                    >
                                         <ChevronRightIcon className="size-6" />
                                     </button>
                                 </div>
