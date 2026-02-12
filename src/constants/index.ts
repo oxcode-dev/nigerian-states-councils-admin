4b3eb287a511fb2a0fe3bb1d1b3a70da6b57d65f
Oxcode <mrexcelsam1@gmail.com>
2026-02-12 10:22:09 +0100

    initial commit

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,7 @@
 export const API_BASE_URL = import.meta.env.VITE_REACT_APP_BACKEND_BASEURL || 'http://localhost:5000/api';
 
 export const STATES_QUERY_KEY = 'list_states';
+export const ALL_STATES_QUERY_KEY = 'list_all_states';
 export const LGAS_QUERY_KEY = 'list_lgas';
 export const WARDS_QUERY_KEY = 'list_wards';
 export const USERS_QUERY_KEY = 'list_users';
---
+++ src/hooks/useFetchAllStates.tsx
@@ -0,0 +1,44 @@
+import { keepPreviousData, useQuery } from '@tanstack/react-query';
+import { get } from '../services';
+import { useMemo } from 'react';
+import { ALL_STATES_QUERY_KEY } from '../constants';
+import type { StateFormProp, StatePaginationProp } from '../types';
+
+export const useFetchAllStates = () => {
+
+    async function fetchAllStates() {
+        const url = `/public/states`;
+    
+        const response = await get(url)
+
+        if (!response.status || response.status !== 201) {
+            throw new Error("Failed to fetch states");
+        }
+
+        return response?.data;
+    }
+
+
+    const { data: stateList, error, isLoading, isFetching } = useQuery({
+        queryKey: [ALL_STATES_QUERY_KEY],
+        queryFn: () => fetchAllStates(),
+        placeholderData: keepPreviousData,
+        staleTime: 10 * 60 * 1000,
+    });
+
+    const states: StateFormProp[] = useMemo(() => {
+        return (stateList as StatePaginationProp)?.states || [];
+    }, [stateList]);
+
+    const metaData = useMemo(() => {
+            return (stateList as StatePaginationProp)?.metadata || null;
+    }, [stateList]);
+
+    return {
+        states,
+        metaData,
+        error,
+        isFetching,
+        isLoading,
+    } 
+}
