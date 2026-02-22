39c31864f831dc977ceb83abff1c8c86f029ebff
Oxcode <mrexcelsam1@gmail.com>
2026-02-22 18:38:05 +0100

    initial commit

--- src/components/EmptyState.tsx
+++ src/components/EmptyState.tsx
@@ -2,7 +2,7 @@ import NoDataImg from "../assets/no-data.svg";
 
 export function EmptyState({ message }: { message: string | null }) {
     return (
-        <div className="w-full h-full flex flex-col items-center justify-center gap-4 p">
+        <div className="w-full h-full flex flex-col items-center justify-center gap-4 md:gap-10 py-20">
             <div className="size-28 md:size-40 flex items-center justify-center">
                 <img src={NoDataImg} className="w-auto h-auto object-cover object-center" />
             </div> 
